69c5a1052041a66a23c79644eadf98c75ad5e71c
Isaac <xiao9aba2008@gmail.com>
2017-11-24 15:21:00 -0800

    fix bug

--- utils/ops.py
+++ utils/ops.py
@@ -77,7 +77,7 @@ def conv_out_block(outs, scope, class_num, is_train, data_format='NHWC'):
             outs, outs.shape[1].value, kernel, scope+'/pure_%s' % i,
             padding='VALID', data_format='NCHW')
         outs = outs if i==2 else tf.nn.relu6(outs, scope+'/pure_%s' % i+'/relu')
-    outs = tf.squeeze(outs, axis=[1, 2], name=scope+'/squeeze')
+    outs = tf.squeeze(outs, axis=[2, 3], name=scope+'/squeeze')
     return outs
 
 
